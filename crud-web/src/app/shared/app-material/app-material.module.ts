84681c1376d156cb6a9100779e7e3284c1283807
ranoob <whyfck@gmail.com>
2023-11-27 19:10:30 -0300

    feat: formfield, input,select and snackbar modules

--- crud-web/src/app/shared/app-material/app-material.module.ts
+++ crud-web/src/app/shared/app-material/app-material.module.ts
@@ -7,6 +7,10 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   exports: [
@@ -17,6 +21,10 @@ import { MatIconModule } from '@angular/material/icon';
     MatDialogModule,
     MatButtonModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSnackBarModule,
   ],
 })
 export class AppMaterialModule {}
