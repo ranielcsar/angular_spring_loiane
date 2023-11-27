bf1bb4b6ea3dcd7b81a9505bebceb0af881c6033
ranoob <whyfck@gmail.com>
2023-11-27 19:09:45 -0300

    feat: save() function

--- crud-web/src/app/courses/services/courses.service.ts
+++ crud-web/src/app/courses/services/courses.service.ts
@@ -6,11 +6,15 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class CoursesService {
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'api/courses';
 
   constructor(private http: HttpClient) {}
 
   list() {
     return this.http.get<Course[]>(this.API);
+  }
+
+  save(record: Course) {
+    return this.http.post<Course>(this.API, record);
   }
 }
