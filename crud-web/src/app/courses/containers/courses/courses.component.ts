c6b24ef32ef32e878b57ac9418299459abb71240
ranoob <whyfck@gmail.com>
2023-11-29 16:46:20 -0300

    move to container

---
+++ crud-web/src/app/courses/containers/courses/courses.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../../model/course';
+import { CoursesService } from '../../services/courses.service';
+import { Observable, catchError, first, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from '../../../shared/components/error-dialog/error-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss'],
+})
+export class CoursesComponent implements OnInit {
+  courses$: Observable<Course[]>;
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
+  }
+
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.courses$ = this.coursesService.list().pipe(
+      first(),
+      catchError((error) => {
+        this.onError('Erro ao carregar cursos.');
+        console.error(error);
+        return of([]);
+      })
+    );
+  }
+
+  ngOnInit(): void {}
+
+  onAdd() {
+    this.router.navigate(['new'], { relativeTo: this.route });
+  }
+
+  onEdit(course: Course) {
+    this.router.navigate(['edit', course._id], { relativeTo: this.route });
+  }
+}
