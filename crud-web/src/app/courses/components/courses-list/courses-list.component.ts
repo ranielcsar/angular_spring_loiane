f86fbb02820be64219165cf3ee865d85fe2fdcd3
ranoob <whyfck@gmail.com>
2023-11-29 16:45:22 -0300

    :sparkles: CourseList component

---
+++ crud-web/src/app/courses/components/courses-list/courses-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Course } from '../../model/course';
+
+@Component({
+  selector: 'app-courses-list',
+  templateUrl: './courses-list.component.html',
+  styleUrls: ['./courses-list.component.scss'],
+})
+export class CoursesListComponent {
+  @Input() courses: Course[] = [];
+  @Output() add = new EventEmitter(false);
+  @Output() edit = new EventEmitter(false);
+  readonly displayedColumns = ['name', 'category', 'actions'];
+
+  onAdd() {
+    this.add.emit(true);
+  }
+
+  onEdit(course: Course) {
+    this.edit.emit(course);
+  }
+
+  onDelete(course: Course) {
+    console.log({ course });
+  }
+}
