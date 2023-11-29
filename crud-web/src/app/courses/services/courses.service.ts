f0b1bbb975a7b39b495f99c84b2cab5879b7d311
ranoob <whyfck@gmail.com>
2023-11-29 16:47:24 -0300

    :recycle: Save method

--- crud-web/src/app/courses/services/courses.service.ts
+++ crud-web/src/app/courses/services/courses.service.ts
@@ -14,7 +14,23 @@ export class CoursesService {
     return this.http.get<Course[]>(this.API);
   }
 
+  loadById(id: string) {
+    return this.http.get<Course>(`${this.API}/${id}`);
+  }
+
   save(record: Partial<Course>) {
+    if (record._id) {
+      return this.update(record);
+    }
+
+    return this.create(record);
+  }
+
+  private create(record: Partial<Course>) {
     return this.http.post<Course>(this.API, record);
+  }
+
+  private update(record: Partial<Course>) {
+    return this.http.put<Course>(`${this.API}/${record._id}`, record);
   }
 }
