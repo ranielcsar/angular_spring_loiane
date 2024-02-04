f71a0a7ffd702d2aef9a6d01f97db0f258892314
ranoob <whyfck@gmail.com>
2024-02-04 10:25:12 -0300

    feat: Course DTO

---
+++ crud-spring/src/main/java/com/ranielcsar/crudspring/dto/CourseDTO.java
@@ -0,0 +1,17 @@
+package com.ranielcsar.crudspring.dto;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record CourseDTO(
+    @JsonProperty("_id") Long id,
+    @NotBlank @NotNull @Length(min = 5, max = 100) String name,
+    @Length(max = 10) @Pattern(regexp = "Back-end|Front-end") @Column(length = 10, nullable = false) String category) {
+
+}
