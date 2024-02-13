6633478b4d2e53be277ffff2744aaf8c7253e77b
ranielcsar <whyfck@gmail.com>
2024-02-13 10:30:54 -0300

    feat: lesson entity and oneToMany relation

--- crud-spring/src/main/java/com/ranielcsar/crudspring/dto/CourseDTO.java
+++ crud-spring/src/main/java/com/ranielcsar/crudspring/dto/CourseDTO.java
@@ -1,8 +1,11 @@
 package com.ranielcsar.crudspring.dto;
 
+import java.util.List;
+
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ranielcsar.crudspring.model.Lesson;
 
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
@@ -10,8 +13,9 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record CourseDTO(
-    @JsonProperty("_id") Long id,
-    @NotBlank @NotNull @Length(min = 5, max = 100) String name,
-    @Length(max = 10) @Pattern(regexp = "Back-end|Front-end") @Column(length = 10, nullable = false) String category) {
+        @JsonProperty("_id") Long id,
+        @NotBlank @NotNull @Length(min = 5, max = 100) String name,
+        @Length(max = 10) @Pattern(regexp = "Back-end|Front-end") @Column(length = 10, nullable = false) String category,
+        List<Lesson> lessons) {
 
 }
--- crud-spring/src/main/java/com/ranielcsar/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/ranielcsar/crudspring/model/Course.java
@@ -1,5 +1,8 @@
 package com.ranielcsar.crudspring.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 import org.hibernate.validator.constraints.Length;
@@ -10,12 +13,14 @@ import com.ranielcsar.crudspring.enums.Status;
 import com.ranielcsar.crudspring.enums.converters.CategoryConverter;
 import com.ranielcsar.crudspring.enums.converters.StatusConverter;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -46,5 +51,9 @@ public class Course {
   @Column(length = 10, nullable = false)
   @Convert(converter = StatusConverter.class)
   private Status status = Status.ACTIVE;
+
+  @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "course")
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+  private List<Lesson> lessons = new ArrayList<>();
 
 }
