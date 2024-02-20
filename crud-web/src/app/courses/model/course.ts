8251996cf80297206a5c49d19e4dab6b83c3362b
ranielcsar <whyfck@gmail.com>
2024-02-20 10:22:54 -0300

    feat: lessons model

--- crud-web/src/app/courses/model/course.ts
+++ crud-web/src/app/courses/model/course.ts
@@ -1,5 +1,10 @@
+import { Lesson } from './lesson';
+
 export interface Course {
   _id: string;
   name: string;
-  category: string;
+  category: CourseCategory;
+  lessons: Lesson[];
 }
+
+export type CourseCategory = 'Front-end' | 'Back-end' | '';
