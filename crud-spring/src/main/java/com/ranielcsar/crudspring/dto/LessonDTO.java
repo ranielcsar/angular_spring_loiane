3be0e1f16d99b3b902efff4ecddf4a0f156beeb7
ranielcsar <whyfck@gmail.com>
2024-03-01 10:10:13 -0300

    refac: remove JsonProperty

--- crud-spring/src/main/java/com/ranielcsar/crudspring/dto/LessonDTO.java
+++ crud-spring/src/main/java/com/ranielcsar/crudspring/dto/LessonDTO.java
@@ -1,10 +1,13 @@
 package com.ranielcsar.crudspring.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record LessonDTO(
-        @JsonProperty("_id") Long id,
-        String name,
-        String youtubeUrl) {
+                Long id,
+                @NotNull @NotBlank @Length(min = 5, max = 100) String name,
+                @NotNull @NotBlank @Length(min = 10, max = 11) String youtubeUrl) {
 
 }
