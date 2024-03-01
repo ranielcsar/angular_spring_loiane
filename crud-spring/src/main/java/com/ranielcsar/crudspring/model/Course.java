00c596c5c796549fc9e10332cac45eaa5babdd58
ranielcsar <whyfck@gmail.com>
2024-03-01 10:10:57 -0300

    fix: remove imports and add validation to lessons list

--- crud-spring/src/main/java/com/ranielcsar/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/ranielcsar/crudspring/model/Course.java
@@ -21,7 +21,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -52,8 +54,11 @@ public class Course {
   @Convert(converter = StatusConverter.class)
   private Status status = Status.ACTIVE;
 
-  @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "course")
+  @NotNull
+  @NotEmpty
+  @Valid
   @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+  @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "course")
   private List<Lesson> lessons = new ArrayList<>();
 
 }
