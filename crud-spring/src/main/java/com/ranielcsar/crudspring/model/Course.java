fa60371e0530244211f0030df884dd6e44c909c5
ranielcsar <whyfck@gmail.com>
2024-03-04 07:56:07 -0300

    refac: remove lombok and add setters/getters

--- crud-spring/src/main/java/com/ranielcsar/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/ranielcsar/crudspring/model/Course.java
@@ -25,9 +25,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
 
-@Data
 @Entity
 @SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
 @SQLRestriction("status <> 'Inativo'")
@@ -57,8 +55,53 @@ public class Course {
   @NotNull
   @NotEmpty
   @Valid
-  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
   @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "course")
   private List<Lesson> lessons = new ArrayList<>();
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Category getCategory() {
+    return category;
+  }
+
+  public void setCategory(Category category) {
+    this.category = category;
+  }
+
+  public Status getStatus() {
+    return status;
+  }
+
+  public void setStatus(Status status) {
+    this.status = status;
+  }
+
+  public List<Lesson> getLessons() {
+    return lessons;
+  }
+
+  public void setLessons(List<Lesson> lessons) {
+    this.lessons = lessons;
+  }
+
+  @Override
+  public String toString() {
+    return "Course [id=" + id + ", name=" + name + ", category=" + category + ", status=" + status + ", lessons="
+        + lessons + "]";
+  }
 
 }
