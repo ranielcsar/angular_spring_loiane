17d3d78170eeb754d06c5cd86cb99c60fa3d80a1
ranielcsar <whyfck@gmail.com>
2024-03-04 08:27:14 -0300

    refac: add enum validation

--- crud-spring/src/main/java/com/ranielcsar/crudspring/dto/CourseDTO.java
+++ crud-spring/src/main/java/com/ranielcsar/crudspring/dto/CourseDTO.java
@@ -5,18 +5,19 @@ import java.util.List;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ranielcsar.crudspring.enums.Category;
+import com.ranielcsar.crudspring.enums.validation.ValueOfEnum;
 
 import jakarta.persistence.Column;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 public record CourseDTO(
-        @JsonProperty("_id") Long id,
-        @NotBlank @NotNull @Length(min = 5, max = 100) String name,
-        @Length(max = 10) @Pattern(regexp = "Back-end|Front-end") @Column(length = 10, nullable = false) String category,
-        @NotNull @NotEmpty @Valid List<LessonDTO> lessons) {
+    @JsonProperty("_id") Long id,
+    @NotBlank @NotNull @Length(min = 5, max = 100) String name,
+    @Length(max = 10) @ValueOfEnum(enumClass = Category.class) @Column(length = 10, nullable = false) String category,
+    @NotNull @NotEmpty @Valid List<LessonDTO> lessons) {
 
 }
