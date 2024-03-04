df63a025c23f565693c15e5263865356f602d10d
ranielcsar <whyfck@gmail.com>
2024-03-04 07:55:32 -0300

    fix: lessons validations

--- crud-spring/src/main/java/com/ranielcsar/crudspring/dto/CourseDTO.java
+++ crud-spring/src/main/java/com/ranielcsar/crudspring/dto/CourseDTO.java
@@ -7,14 +7,16 @@ import org.hibernate.validator.constraints.Length;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record CourseDTO(
-    @JsonProperty("_id") Long id,
-    @NotBlank @NotNull @Length(min = 5, max = 100) String name,
-    @Length(max = 10) @Pattern(regexp = "Back-end|Front-end") @Column(length = 10, nullable = false) String category,
-    List<LessonDTO> lessons) {
+        @JsonProperty("_id") Long id,
+        @NotBlank @NotNull @Length(min = 5, max = 100) String name,
+        @Length(max = 10) @Pattern(regexp = "Back-end|Front-end") @Column(length = 10, nullable = false) String category,
+        @NotNull @NotEmpty @Valid List<LessonDTO> lessons) {
 
 }
