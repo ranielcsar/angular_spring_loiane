d182af93c4f6ca055175d40db111025fd80d2eca
ranielcsar <whyfck@gmail.com>
2024-03-04 07:55:16 -0300

    feat: add 3 new exceptions

--- crud-spring/src/main/java/com/ranielcsar/crudspring/controller/ApplicationControllerAdvice.java
+++ crud-spring/src/main/java/com/ranielcsar/crudspring/controller/ApplicationControllerAdvice.java
@@ -1,18 +1,46 @@
 package com.ranielcsar.crudspring.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.ranielcsar.crudspring.exception.RecordNotFoundException;
 
+import jakarta.validation.ConstraintViolationException;
+
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
-    
+
     @ExceptionHandler(RecordNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleNotFoundException(RecordNotFoundException exception) {
         return exception.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        return exception.getFieldErrors().stream().map(err -> err.getField() + " " + err.getDefaultMessage())
+                .reduce("", (acc, err) -> acc + err + "\n");
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleConstraintViolationException(ConstraintViolationException exception) {
+        return exception.getConstraintViolations().stream().map(err -> err.getPropertyPath() + " " + err.getMessage())
+                .reduce("", (acc, err) -> acc + err + "\n");
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception) {
+        if (exception != null && exception.getRequiredType() != null) {
+            return exception.getName() + " should be of type " + exception.getRequiredType().getSimpleName();
+        }
+
+        return "Invalid argument type";
     }
 }
