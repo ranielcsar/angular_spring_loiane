6de7a4b88d3aaec9d330cdcf39959cffebd252d2
ranielcsar <whyfck@gmail.com>
2024-04-04 05:58:06 -0300

    standalone components migration - modules

--- crud-web/src/app/courses/courses.module.ts
+++ crud-web/src/app/courses/courses.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './containers/courses/courses.component';
-import { AppMaterialModule } from '../shared/app-material/app-material.module';
-import { SharedModule } from '../shared/shared.module';
+
+
 import { CourseFormComponent } from './containers/course-form/course-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CoursesListComponent } from './components/courses-list/courses-list.component';
@@ -13,12 +13,10 @@ import { CoursesListComponent } from './components/courses-list/courses-list.com
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    AppMaterialModule,
-    SharedModule,
     ReactiveFormsModule,
     CoursesComponent,
     CourseFormComponent,
     CoursesListComponent,
-  ],
+],
 })
 export class CoursesModule {}
